2cbf2d4e164418afbf658da2759802e84d18f1cf
vasyl <vasyl@flash-fx.com.au>
2022-03-25 14:05:44 +1100

    Fix up index.d.ts with differences from the origin

--- index.d.ts
+++ index.d.ts
@@ -2,12 +2,13 @@ import { IncomingMessage, ServerResponse } from "http";
 import { GraphQLScalarType } from "graphql";
 import { RequestHandler } from "express";
 import { DefaultContext, DefaultState, Middleware } from "koa";
-import { ReadStream } from 'fs';
+import { ReadStream } from "fs";
 
 export interface UploadOptions {
   maxFieldSize?: number | undefined;
   maxFileSize?: number | undefined;
   maxFiles?: number | undefined;
+  environment?: "lambda" | "gcf" | "azure";
 }
 
 export interface GraphQLOperation {
@@ -26,14 +27,16 @@ export function graphqlUploadExpress(
   uploadOptions?: UploadOptions
 ): RequestHandler;
 
-export function graphqlUploadKoa <StateT = DefaultState, ContextT = DefaultContext>(
-  uploadOptions?: UploadOptions
-): Middleware<StateT, ContextT>;
+export function graphqlUploadKoa<
+  StateT = DefaultState,
+  ContextT = DefaultContext
+>(uploadOptions?: UploadOptions): Middleware<StateT, ContextT>;
 
 export const GraphQLUpload: GraphQLScalarType;
 
 export interface FileUpload {
   filename: string;
+  fieldName: string;
   mimetype: string;
   encoding: string;
   createReadStream(): ReadStream;
